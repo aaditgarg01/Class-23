cf3f0d0e721b21a70e07af80ed57a18cb549003e
aaditgarg01 <77445321+aaditgarg01@users.noreply.github.com>
2021-02-10 18:43:08 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box
+{
+  constructor(x,y,width,height)
+  {
+      var options = {
+          restitution:1
+      }
+      this.body=Bodies.rectangle(x,y,width,height,options);
+      World.add(world,this.body);
+  }  
+  display()
+  {
+      var pos = this.body.position
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width,this.height)
+  }
+}
